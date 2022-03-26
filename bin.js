6da8074e58fd9c07a8dfd211f2883f20c13a3fb3
ebrugulec <gulecebrueg@gmail.com>
2022-03-26 19:30:18 +0300

    Remove unnessasry packages and add log

--- bin.js
+++ bin.js
@@ -4,24 +4,26 @@ const chalk = require('chalk');
 const shell = require('shelljs')
 const clear = require('clear');
 const figlet = require('figlet');
+const CLI = require('clui');
+
+const log = console.log;
 
 clear();
 
-console.log(
+log(
   chalk.yellow(
     figlet.textSync('CheatSheet', { horizontalLayout: 'full' })
   )
 )
 
-const CLI = require('clui');
 const Spinner = CLI.Spinner;
 
 const status = new Spinner('Project is installing, please wait.');
 status.start();
 
-//Parent path
-const folderPath = process.cwd().split("/").slice(0, -1).join("/")
-shell.cd(folderPath)
+//path
+const path = process.cwd()
+shell.cd(path)
 
 //Clone repository
 shell.exec('git clone https://github.com/ebrugulec/simple-app.git')
@@ -32,3 +34,14 @@ shell.exec('npm i')
 
 status.stop();
 
+log(chalk.green('Success!'));
+log(chalk.white('We suggest that you begin by typing:\n'));
+
+log(chalk.blue('cd') + ' simple-app');
+log(chalk.blue('yarn dev'));
+
+log(chalk.white('\n\nInside that directory, you can run several commands:\n'));
+
+log(chalk.blue('yarn dev') + chalk.white('\n\tStarts the development server.'))
+log(chalk.blue('yarn build') + chalk.white('\n\tBuilds the app for production.'))
+log(chalk.blue('yarn start') + chalk.white('\n\tRuns the built app in production mode.'))
